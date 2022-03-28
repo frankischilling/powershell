11720da1b13f63d46701181b2c3de223814f0d61
frankischilling <frankhagan890@gmail.com>
2022-03-28 08:06:57 -0400

    tghjkl

--- fun.ps1
+++ fun.ps1
@@ -2,3 +2,14 @@ Clear-Host
 $test = Get-Process | Get-Process | select name, cpu, TotalProcessorTime | Sort-Object -property TotalProcessorTime -Descending
 Out-File -FilePath .\hi.txt
 $test | Out-File .\hi.txt 
+
+$uinput = Read-Host "what is ur fav number"
+Write-Output "You're fav number is $uinput"
+
+
+if ($uinput -eq [int]) {
+    Write-Host "thx"
+} else {
+    write-host "xD no"
+}
+
