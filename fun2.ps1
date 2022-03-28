609b5725e2b9b523db62a6cdedd372325c9475c5
frankischilling <frankhagan890@gmail.com>
2022-03-28 09:13:07 -0400

    fun

--- fun2.ps1
+++ fun2.ps1
@@ -1,6 +1,7 @@
 Clear-Host # clears
 $time = Get-Date
 Write-Host "Executed @ $time" -BackgroundColor red -ForegroundColor white # writes when it has been ran
-for ($counter =1; $counter -le 25; $counter++) { #counter starts at one and checks until its less then or equal to 25 
+for ($counter =1; $counter -le 10; $counter++) { #counter starts at one and checks until its less then or equal to 25 
     Write-Host "The Number Is: $counter" # writes what number the counter is at 
 }
+Write-Host "Counter ended @ $time" -BackgroundColor red -ForegroundColor white
