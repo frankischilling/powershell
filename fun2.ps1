3e332ee2b0d7cab1cc988f1c829f40f4bd00e075
frankischilling <frankhagan890@gmail.com>
2022-03-28 08:38:39 -0400

    counter

--- fun2.ps1
+++ fun2.ps1
@@ -1,4 +1,6 @@
-$count = 5.747742
-$count = $count *7890317878937890120.7896423478814556768796213497340347
-
-[Math]::Floor($count)
+Clear-Host # clears
+$time = Get-Date
+Write-Host "Executed @ $time" -BackgroundColor red -ForegroundColor white # writes when it has been ran
+for ($counter =1; $counter -le 25; $counter++) { #counter starts at one and checks until its less then or equal to 25 
+    Write-Host "The Number Is: $counter" # writes what number the counter is at 
+}
