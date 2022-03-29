be61e154117d2140351d83fdfb9dc344df7a63a2
frankischilling <frankhagan890@gmail.com>
2022-03-29 08:25:20 -0400

    ghjkl;

--- fun2.ps1
+++ fun2.ps1
@@ -1,7 +1,17 @@
 Clear-Host # clears
 $time = Get-Date
 Write-Host "Executed @ $time" -BackgroundColor red -ForegroundColor white # writes when it has been ran
-for ($counter =1; $counter -le 10; $counter++) { #counter starts at one and checks until its less then or equal to 25 
-    Write-Host "The Number Is: $counter" # writes what number the counter is at 
-}
+    $input = 0
+    do {
+        $input = [int]::TryParse(($ask = Read-Host "Enter the factor"), [ref]$input)
+        if (-not $input) {
+            Write-Host "Please use a number"
+        } else {
+        $factor = $ask
+        for ($counter =1; $counter -le 50; $counter++) { #counter starts at one and checks until its less then or equal to 25 
+         $answer = $counter*$factor
+         Write-Host " $factor times $counter equals $answer" # writes what number the counter is at 
+        }
+    }
+    } while (-not $input)
 Write-Host "Counter ended @ $time" -BackgroundColor red -ForegroundColor white
